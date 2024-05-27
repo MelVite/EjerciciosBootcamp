99ce68990812b926967e0b6d25a012fb1007fa93
MelVite <melanievite38@gmail.com>
2024-05-27 15:00:12 -0600

    Corrección de lógica

--- Live-coding 4/numeros.java
+++ Live-coding 4/numeros.java
@@ -4,13 +4,13 @@ public class numeros {
 	
 	public void printNum() {
 	
-		for (int i = 0 ; i <= 100; i++) {
-			if (i % 3 == 0) {
-				System.out.println("Fizz");
+		for (int i = 1 ; i <= 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
 			} else if (i % 5 == 0) {
 				System.out.println("Buzz");
-			} else if ((i % 3 == 0) && (i % 5 == 0)) {
-				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
 			} else {
 				System.out.println(i);
 			}
